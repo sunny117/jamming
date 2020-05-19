743c143a1e0b8d480623a50b1173306ef498fa22
Srikar Reddy <srikarreddymadaka@gmail.com>
2020-05-19 22:21:52 +0530

    implement spotify search request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import SearchBar from ',,/SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state.searchResults = [{
       name: '',
@@ -26,47 +27,49 @@ class App extends React.Component {
     this.savePlaylist = this.savePlaylist.bind(this);
     this.search = this.search.bind(this);
   }
-  addTrack(track){
-    if(this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
     }
     this.setState({
       playlistTracks: this.state.playlistTracks.push(track)
     });
   }
-  removeTrack(track){
-    if(this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+  removeTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
       this.setState({
-        playlistTracks: this.state.playlistTracks.filter(item => item!=track)
+        playlistTracks: this.state.playlistTracks.filter(item => item != track)
       })
     }
   }
-  updatePlaylistName(name){
+  updatePlaylistName(name) {
     this.setState({
       playlistName: name
     });
   }
-  savePlaylist(){
+  savePlaylist() {
     const trackURIs;
     //tO be done
   }
-  search(term){
-    console.log(term);
+  search(term) {
+    this.setState({
+      searchResults: Spotify.search(term)
+    });
   }
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar onSearch={this.search}/>
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}
-                           onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} 
-                      playlistTracks={this.state.playlistTracks} 
-                      onRemove={this.removeTrack}
-                      onNameChange={this.updatePlaylistName}
-                      onSave={this.savePlaylist}/>
+              onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,5 @@
+import SearchBar from "../Components/SearchBar/SearchBar";
+
 const CLIENT_ID = '165167bfcf5942508af17ba9f47dfbe8';
 const REDIRECT_URI = 'http://localhost:3000/';
 const accessToken = '';
@@ -16,6 +18,27 @@ const Spotify = {
         if(accessToken == ''){
             window.location.href = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
         }
+    },
+
+    Search(term){
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
+            headers: {Authorization: `Bearer ${accessToken}`}
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(jsonResponse.tracks){
+                return jsonResponse.tracks.map(track => {
+                    return {
+                        id: track.id,
+                        name: track.name,
+                        artist: track.artists[0].name,
+                        album: track.album.name,
+                        uri: track.uri
+                    }
+                });
+            }
+            return [];
+        });
     }
 
 }
