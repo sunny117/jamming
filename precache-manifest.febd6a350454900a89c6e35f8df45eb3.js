2e3f27839f7a55a8f5b70b42699bfd722606d5d4
Srikar Reddy <srikarreddymadaka@gmail.com>
2020-05-21 12:51:10 +0530

    Updates

---
+++ precache-manifest.febd6a350454900a89c6e35f8df45eb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300530b561b5c02b5d07d131ce4b8874",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "6b54b98f591ddbd94396",
+    "url": "/jamming/static/css/main.e56cc963.chunk.css"
+  },
+  {
+    "revision": "85e29eff6dce0436f680",
+    "url": "/jamming/static/js/2.eb9a55cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jamming/static/js/2.eb9a55cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b54b98f591ddbd94396",
+    "url": "/jamming/static/js/main.889df456.chunk.js"
+  },
+  {
+    "revision": "0f3bd63a3706852ca01f",
+    "url": "/jamming/static/js/runtime-main.76fad94b.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
